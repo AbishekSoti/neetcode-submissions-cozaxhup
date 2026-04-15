6474f57c4ccab5472071c91ac57c3a7974f26465
Abishek Soti <85747087+AbishekSoti@users.noreply.github.com>
2026-04-15 17:59:47 +1000

    Add: longest-common-prefix - submission-41

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-41.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
+        first_word = strs[0]
+
+
+        for i in range(1,len(strs)):
+            shortestlength= min(len(first_word), len(strs[i]))
+            first_word = first_word[:shortestlength]
+            for j in range(0,len(first_word)):
+                
+                if first_word == "":
+                    break
+                if first_word[j]!=strs[i][j]:
+                    #drop the letter from the list.
+                    first_word = first_word[:j]
+                    break
+
+        return first_word
+            
