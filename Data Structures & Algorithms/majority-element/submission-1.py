00f71ca3c028f6428a93f3d4c62a091a067b64f9
Abishek Soti <85747087+AbishekSoti@users.noreply.github.com>
2026-04-16 02:52:46 +1000

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count_dict = {x:nums.count(x) for x in nums if nums.count(x)>=len(nums)/2}
+
+        print(count_dict)
+        
+        for key in count_dict:
+            return key
