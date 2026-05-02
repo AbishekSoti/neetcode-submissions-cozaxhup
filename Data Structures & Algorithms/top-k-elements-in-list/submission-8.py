13aa3cfc23e16b8237707bd77ea95c2d797c917c
Abishek Soti <85747087+AbishekSoti@users.noreply.github.com>
2026-05-03 09:13:35 +1000

    Add: top-k-elements-in-list - submission-8

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.py
@@ -0,0 +1,22 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        ordered_dict = {}
+        for i in range(0,len(nums)):
+            if nums[i] not in ordered_dict:
+                ordered_dict[nums[i]] = 1
+            else:
+                ordered_dict[nums[i]] +=1
+        print(ordered_dict)
+
+        # Create a list with index as the keys( no index = empty list) and values would be the frequency.
+        frequency = [[] for x in range(len(nums)+1)]
+        print(frequency)
+        for key,value in ordered_dict.items():
+            frequency[value].append(key)
+        print(frequency)
+
+        empty_bucket = []       
+        for i in reversed(frequency):
+            #if i is not []:
+            empty_bucket+=i
+        return empty_bucket[:k]
