ce293f33f116e6adbadf81dd02f8d194b26d98c9
Abishek Soti <85747087+AbishekSoti@users.noreply.github.com>
2026-08-29 22:30:10 +1000

    Add: majority-element-ii - submission-1

---
+++ Data Structures & Algorithms/majority-element-ii/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        nums_dict = {}
+        n = len(nums)
+        for i in nums:
+            if i not in nums_dict:
+                nums_dict[i] = 1
+            else:
+                nums_dict[i]+=1              
+        print(nums_dict)
+        nums_dict
+        final_list = []
+
+        for key, value in nums_dict.items():
+            if value> n/3:
+                final_list.append(key)
+        return final_list
