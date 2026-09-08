b8c684da8e79f079114ea2c6265674d22445f7c6
Abishek Soti <85747087+AbishekSoti@users.noreply.github.com>
2026-09-08 14:49:21 +1000

    Add: reverse-string - submission-2

---
+++ Data Structures & Algorithms/reverse-string/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        # odd vs even. might not matter
+        start = 0
+        end = len(s)-1
+        i = 0
+        # only need to iterate half time. while loop.
+        while start<end:
+            s[start],s[end] = s[end],s[start]
+            start+=1
+            end-=1
+        
